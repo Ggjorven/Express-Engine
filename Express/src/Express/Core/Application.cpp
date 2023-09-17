c88d6c16c08d978f6ad8407ae54c543a5490d649
Ggjorven <Jorbenvanderwal@gmail.com>
2023-09-17 17:44:47 +0200

    Initial commit.

---
+++ Express/src/Express/Core/Application.cpp
@@ -0,0 +1,18 @@
+#include "expch.h"
+#include "Application.hpp"
+
+namespace Express
+{
+	Application::Application()
+	{
+	}
+	Application::~Application()
+	{
+	}
+	void Application::Run()
+	{
+	}
+	void Application::Init()
+	{
+	}
+}
---
+++ Express/src/Express/Express.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+//Express Engine header files
+#include "Express/Core/Core.hpp"
+#include "Express/Core/Application.hpp"
+
+
+//---Express-Engine---//
+#include "Express/Core/EntryPoint.hpp"
+//--------------------//
